41dfca378dc222029473cc831a2d89d07e3741a7
Alice-33 <2643429561@qq.com>
2021-10-09 16:30:02 +0800

    1009 afternoon

---
+++ react_staging/12src_解决样式丢失问题/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        // console.log('Home接收到的props',this.props);
+        return (
+            <h3>
+               我是Home的内容 
+            </h3>
+        )
+    }
+}
---
+++ react_staging/13src_精准匹配与模糊匹配/App.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Headers from './components/Header';
+import MyNavLink from './components/MyNavLink';
+
+export default class App extends Component {
+
+    // 得有一个方法用来更新保存下来的用户
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <Headers />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+                            {/* 原生HTML中靠a标签进行页面的跳转 */}
+                            {/* <a className="list-group-item active" href="./about.html">About</a>
+          <a className="list-group-item" href="./home.html">Home</a> */}
+                            {/* 在react中靠路由链接切换组件 ,to表示要跳转到哪里*/}
+                            {/* 编写路由链接 */}
+                            <MyNavLink to='/about'>About</MyNavLink>
+                            <MyNavLink to='/home/a/b' >Home</MyNavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                {/* 注册路由 */}
+                                <Switch>
+                                    <Route exact={true} path='/about' component={About} />
+                                    <Route path='/home' component={Home} />
+                                </Switch>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
