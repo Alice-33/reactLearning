3baa0e9a81c5cdac1e7f6667e6bda3f8d129bce2
Alice-33 <2643429561@qq.com>
2021-10-11 16:34:38 +0800

    1010 react

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,46 +1,14 @@
-import React, { Component } from 'react';
-import { Route, Switch,Redirect } from 'react-router-dom';
-import About from './pages/About';
-import Home from './pages/Home';
-import Headers from './components/Header';
-import MyNavLink from './components/MyNavLink';
+import React, { Component } from 'react'
+import { Button } from 'antd';
+import { RocketOutlined } from '@ant-design/icons';
 
 export default class App extends Component {
-
-    // 得有一个方法用来更新保存下来的用户
     render() {
         return (
             <div>
-                <div className="row">
-                    <div className="col-xs-offset-2 col-xs-8">
-                        <Headers />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-2 col-xs-offset-2">
-                        <div className="list-group">
-                            {/* 原生HTML中靠a标签进行页面的跳转 */}
-                            {/* <a className="list-group-item active" href="./about.html">About</a>
-          <a className="list-group-item" href="./home.html">Home</a> */}
-                            {/* 在react中靠路由链接切换组件 ,to表示要跳转到哪里*/}
-                            {/* 编写路由链接 */}
-                            <MyNavLink to='/about'>About</MyNavLink>
-                            <MyNavLink to='/home' >Home</MyNavLink>
-                        </div>
-                    </div>
-                    <div className="col-xs-6">
-                        <div className="panel">
-                            <div className="panel-body">
-                                {/* 注册路由 */}
-                                <Switch>
-                                    <Route path='/about' component={About} />
-                                    <Route path='/home' component={Home} />
-                                    <Redirect to='/about'/>
-                                </Switch>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                App_
+                <Button type="primary">点击</Button>
+                <RocketOutlined />
             </div>
         )
     }
